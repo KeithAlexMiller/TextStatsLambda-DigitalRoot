25373809454eb908e96f6448bfcbfe13c187a3b2
Keith Miller <keithalexmiller@gmail.com>
2015-04-20 20:52:47 -0600

    DigitalRoot Completed

--- TextStatsLambda-DigitalRoot/TextStatsLambda-DigitalRoot/Program.cs
+++ TextStatsLambda-DigitalRoot/TextStatsLambda-DigitalRoot/Program.cs
@@ -11,11 +11,49 @@ namespace TextStatsLambda_DigitalRoot
         static void Main(string[] args)
         {
 
+            Console.ReadKey();
         }
 
         public static int DigitalRoot(string rootThisNumber)
         {
-            return 0;
+            int sum = 0;
+            int digRoot = 0;
+            int temp = 0;
+            //ForEach(i => rootThisNumber.Add(Int32.Parse(i))); // maybe Convert.ToInt32() is better?
+
+            List<int> numbers = new List<int>(rootThisNumber.Length);
+            foreach (char c in rootThisNumber)
+            {
+                temp = (c - '0');
+                sum += temp;
+            }
+
+            //handles when sum is equal to digRoot
+            if (sum < 10)
+            {
+                digRoot = sum;
+                return digRoot;
+            }
+
+            //hanldes edge case of sum of 10
+            else if (digRoot == 10)
+            {
+                digRoot = 1;
+            }
+            
+            //hanldes cases over ten where both digits in sum need to be added to each other
+            else if (sum > 10)
+            {
+                temp = 0;
+                string sumString = sum.ToString();
+
+                foreach (char c in sumString)
+                {
+                    temp = (c - '0');
+                    digRoot += temp;
+                }
+            }
+            return digRoot;
         }
 
         public static int NumberOfWords(string inputString)
